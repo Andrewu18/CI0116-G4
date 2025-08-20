a6254e70d49bb9ef356e8dfd02eb196613dbe672
Andrewu18 <56287760+Andrewu18@users.noreply.github.com>
2025-08-20 14:23:37 -0600

    Implementacion final del codigo base para ordenamiento Bubble Sort en hileras de 10, 100 y 1000 elementos

--- Tareas/Tarea 1/bubbleSortBase.py
+++ Tareas/Tarea 1/bubbleSortBase.py
@@ -19,30 +19,50 @@ def bubble_sort(hilera, tamano) :
                   hilera[j], hilera[j + 1] = hilera[j + 1], hilera[j]
     
     
-#main de prueba
+#main
 def main():
+    #Aqui se solicita al usuario si desea ver las hileras ya que imprimirlas abarca mucho espacio (funciona para pruebas e informacion general)
+    ver_hileras = input("Desea ver los contenidos de las hileras antes y despues de la ejecucion de Bubble Sort?   (s/n): "  ).strip().lower() == "s"
     #Itera 5 veces para obtener los tiempos de 5 ejecuciones
     for z in range(5) : 
-         #Hace 3 hileras de 10, 100 y 1000 elementos aleatorios con enteros del 1 al 1000
-        #Imprime el estado de estas hileras antes y despues de aplicar bubble_sort
+        print("Iteracion numero: ", z + 1)
+        print()
+        #Hace 3 hileras de 10, 100 y 1000 elementos aleatorios con enteros del 1 al 1000
+        #Imprime el estado de estas hileras antes y despues de aplicar bubble_sort si se escogio imprimirlas
+        #Imprime el tiempo de ejecucion del algoritmo en la hilera actual
         print("Hilera de 10 numeros al azar antes y despues de aplicar bubble sort:")
         hilera10 = hilera_numeros_aleatorios(10, 1, 1000)
-        print(hilera10)
+        if ver_hileras:
+            print(hilera10)
+        inicio = time.perf_counter()
         bubble_sort(hilera10, 10)
-        print(hilera10)
+        fin = time.perf_counter()
+        if ver_hileras:
+            print(hilera10)
+        print("El tiempo de ejecucion de Bubble Sort en la hilera de 10 elementos fue de: ", format(fin - inicio), "segundos")
+        print()
         print("Hilera de 100 numeros al azar antes y despues de aplicar bubble sort:")
         hilera100 = hilera_numeros_aleatorios(100, 1, 1000)
-        print(hilera100)
+        if ver_hileras:
+            print(hilera100)
+        inicio = time.perf_counter()
         bubble_sort(hilera100, 100)
-        print(hilera100)
+        fin = time.perf_counter()
+        if ver_hileras:
+            print(hilera100)
+        print("El tiempo de ejecucion de Bubble Sort en la hilera de 100 elementos fue de: ", format(fin - inicio), "segundos")
+        print()
         print("Hilera de 1000 numeros al azar antes y despues de aplicar bubble sort:")
         hilera1k = hilera_numeros_aleatorios(1000, 1, 1000)
-        print(hilera1k)
+        if ver_hileras:
+            print(hilera1k)
+        inicio = time.perf_counter()
         bubble_sort(hilera1k, 1000)
-        print(hilera1k)
-        print("Esta fue la iteracion numero: ", z + 1)
-        z + 1
-
+        fin = time.perf_counter()
+        if ver_hileras:
+            print(hilera1k)
+        print("El tiempo de ejecucion de Bubble Sort en la hilera de 1000 elementos fue de: ", format(fin - inicio), "segundos")
+        print()
     
 if __name__ == "__main__":
     main()
